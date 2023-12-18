81802327242ad762e2e5764a926d55cc02bcb8d1
Lucas Dantas <lucasdantasguitar@gmail.com>
2023-12-18 16:31:07 -0300

    style: created newsletter container

--- src/app/components/footer/footer-social-content/footer-social-content.component.ts
+++ src/app/components/footer/footer-social-content/footer-social-content.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-footer-social-content',
+  selector: "app-footer-social-content",
   standalone: true,
   imports: [],
-  templateUrl: './footer-social-content.component.html',
-  styleUrl: './footer-social-content.component.css'
+  templateUrl: "./footer-social-content.component.html",
+  styleUrl: "./footer-social-content.component.css",
 })
 export class FooterSocialContentComponent {
-    socialIcons: string[] = ["assets/facebook-icon.svg", "assets/twitter-icon.svg", "assets/linkedin-icon.svg"]
+  socialIcons: string[] = [
+    "assets/facebook-icon.svg",
+    "assets/twitter-icon.svg",
+    "assets/linkedin-icon.svg",
+  ];
 }
