3e72d146308ad17e80795377fed73c2be3a79260
Lucas Dantas <lucasdantasguitar@gmail.com>
2023-12-19 09:33:12 -0300

    feat: added minimal contents and styling, in about-us/contact pages

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
